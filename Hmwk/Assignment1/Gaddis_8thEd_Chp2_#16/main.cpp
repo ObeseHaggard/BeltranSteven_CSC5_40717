7c2cab962eadd9cc01663523dfa41466f689e7ad
sbeltran94 <steven_beltran@rocketmail.com>
2015-01-08 08:54:57 -0800

    Signed-off-by: sbeltran94 <steven_beltran@rocketmail.com>

---
+++ Hmwk/Assignment1/Gaddis_8thEd_Chp2_#16/main.cpp
@@ -0,0 +1,30 @@
+/* 
+ * File:   main.cpp
+ * Author: Steven Beltran
+ * Purpose: Hmwk to Diamond Pattern
+ * Created on January 8, 2015, 8:49 AM
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    cout<<"   *   "<<endl;
+    cout<<"  ***  "<<endl;
+    cout<<" ***** "<<endl;
+    cout<<"*******"<<endl;
+    cout<<" ***** "<<endl;
+    cout<<"  ***  "<<endl;
+    cout<<"   *   "<<endl;
+
+    return 0;
+}
+
---
+++ Savitch_9thEd_Chp1_ProgProj5/main.cpp
@@ -0,0 +1,29 @@
+/* 
+ * File:   main.cpp
+ * Author: Steven Beltran
+ * Purpose: Hmwk to Savitch ProgProj 5
+ * Created on January 8, 2015, 8:38 AM
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution begins here
+int main(int argc, char** argv) {
+    //Declare Variables
+    char c;//character
+    
+    //Prompt user for a character
+    cout<<"This Program will output a large block letter"<<endl;
+    cout<<"With a character you input"<<endl;
+    
+    return 0;
+}
+
