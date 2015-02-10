5b1f850af95b340b891f69175d4e309fedac1bb9
sbeltran94 <steven_beltran@rocketmail.com>
2015-02-09 18:44:47 -0800

    Signed-off-by: sbeltran94 <steven_beltran@rocketmail.com>

---
+++ Project2/Project2/main.cpp
@@ -0,0 +1,25 @@
+/* 
+ * File:   main.cpp
+ * Author: Steven Beltran
+ * Purpose: Project 2 Blackjack
+ * Created on February 9, 2015, 6:41 PM
+ */
+
+//System Libraries
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution begins here
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
